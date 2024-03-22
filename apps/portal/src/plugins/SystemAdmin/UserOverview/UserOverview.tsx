ee194d901c265fbb26c9608310eb3fe7cc20f9ff
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2024-03-22 14:37:27 +0800

    394 remove NiFi admin in UI (#14)

--- apps/portal/src/plugins/SystemAdmin/UserOverview/UserOverview.tsx
+++ apps/portal/src/plugins/SystemAdmin/UserOverview/UserOverview.tsx
@@ -15,7 +15,6 @@ import {
   Roles,
   ALP_ROLES,
   ALP_SYSTEM_ADMIN,
-  ALP_NIFI_ADMIN,
   ALP_SQLEDITOR_ADMIN,
   ALP_USER_ADMIN,
   DATA_ADMIN_ROLES,
@@ -151,10 +150,6 @@ export const UserOverview: FC<UserOverviewProps> = () => {
 
     if (roles.includes(ALP_SYSTEM_ADMIN)) {
       roleList.push(DATA_ADMIN_ROLES[ALP_SYSTEM_ADMIN]);
-    }
-
-    if (roles.includes(ALP_NIFI_ADMIN)) {
-      roleList.push(DATA_ADMIN_ROLES[ALP_NIFI_ADMIN]);
     }
 
     if (roles.includes(ALP_SQLEDITOR_ADMIN)) {
