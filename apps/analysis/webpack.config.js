0540c8a9ff04bc3eef0a5e2b4951e96928712fd3
hengxian-jiang <hengxian.jiang@u.nus.edu>
2024-04-09 16:57:21 +0800

    update webpack.config to remove starboard dependency

--- apps/analysis/webpack.config.js
+++ apps/analysis/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
   const isBuild = env.build
 
   const entryFile = isBuild ? 'module.ts' : 'index.tsx'
-  const externals = isBuild ? ['react', '@alp-os/alp-starboard-wrap'] : []
+  const externals = isBuild ? ['react'] : []
 
   return {
     mode: argv.mode,
