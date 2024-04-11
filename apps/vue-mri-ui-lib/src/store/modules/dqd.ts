e78e0beee01cdfe10f9c79a40df0099c76533444
hengxian-jiang <hengxian.jiang@u.nus.edu>
2024-04-11 11:00:29 +0800

    fix dqd run via PA url

--- apps/vue-mri-ui-lib/src/store/modules/dqd.ts
+++ apps/vue-mri-ui-lib/src/store/modules/dqd.ts
@@ -5,6 +5,7 @@ let cancel
 const dataflow_mgmt_url = '/dataflow-mgmt/'
 
 const actions = {
+  // TODO: check if this endpoint is deprecated
   async fetchDataQualityFlowRun({ commit, dispatch, rootGetters }, { cohortDefinitionId }) {
     let url = `${dataflow_mgmt_url}dqd/data-quality/dataset/${rootGetters.getSelectedUserStudy.id}/cohort/${cohortDefinitionId}/flow-run/latest`
     return dispatch('ajaxAuth', { url, method: 'GET' })
@@ -23,7 +24,7 @@ const actions = {
       cancel = c
     })
 
-    let url = `${dataflow_mgmt_url}dqd/data-quality/flow-run`
+    let url = `${dataflow_mgmt_url}prefect/flow-run/metadata`
     return dispatch('ajaxAuth', { url, method: 'POST', params: GenerateDataQualityFlowRunParams, cancelToken })
       .then(response => {
         return response.data
