6852c860ff9b54098a2a91d97c108d7828198db1
Jerome Ng <jerome.ng@data4life-asia.care>
2024-04-18 07:55:35 +0800

    update

--- apps/portal/src/components/Charts/Reports/Person/Person.tsx
+++ apps/portal/src/components/Charts/Reports/Person/Person.tsx
@@ -10,12 +10,14 @@ import { parsePieChartData, parseBarChartData } from "../../util";
 
 import { PERSON_REPORT_TYPE, WEBAPI_CDMRESULTS_SOURCE_KEYS } from "../../../DQD/types";
 import "./Person.scss";
+import { TranslationContext } from "../../../../contexts/TranslationContext";
 
 interface PersonProps {
   flowRunId: string;
 }
 
 const Person: FC<PersonProps> = ({ flowRunId }) => {
+  const { getText, i18nKeys } = TranslationContext();
   const [personData, setPersonData] = useState<PERSON_REPORT_TYPE>({
     population: [],
     gender: [],
@@ -37,7 +39,7 @@ const Person: FC<PersonProps> = ({ flowRunId }) => {
     } catch (error) {
       console.error(error);
       setIsLoadingPersonData(false);
-      setErrPerson(`Error occured when fetching data characterization person data`);
+      setErrPerson(getText(i18nKeys.PERSON__ERROR_MESSAGE));
     }
   }, [flowRunId]);
 
@@ -49,22 +51,25 @@ const Person: FC<PersonProps> = ({ flowRunId }) => {
   return (
     <>
       {isloadingPersonData ? (
-        <Loader text="Loading Person Reports" />
+        <Loader text={getText(i18nKeys.PERSON__LOADER)} />
       ) : errPerson ? (
         <div className="info__section">{errPerson}</div>
       ) : (
         <>
           <BarChart
             barChartData={parseBarChartData(personData.yearOfBirthData, personData.yearOfBirthStats[0].MINVALUE)}
-            title="Year of Birth"
-            xAxisName="Year"
-            yAxisName="# of Persons"
-            tooltipFormat="Year: {b}<br /># of Persons: {c}"
+            title={getText(i18nKeys.PERSON__BAR_CHART_TITLE)}
+            xAxisName={getText(i18nKeys.PERSON__BAR_CHART_X_AXIS_NAME)}
+            yAxisName={getText(i18nKeys.PERSON__BAR_CHART_Y_AXIS_NAME)}
+            tooltipFormat={getText(i18nKeys.PERSON__BAR_CHART_TOOLTIP_FORMAT)}
           />
           <div className="chart__container">
-            <PieChart data={parsePieChartData(personData.gender)} title="Gender" />
-            <PieChart data={parsePieChartData(personData.race)} title="Race" />
-            <PieChart data={parsePieChartData(personData.ethnicity)} title="Ethnicity" />
+            <PieChart data={parsePieChartData(personData.gender)} title={getText(i18nKeys.PERSON__PIE_CHART_1_TITLE)} />
+            <PieChart data={parsePieChartData(personData.race)} title={getText(i18nKeys.PERSON__PIE_CHART_2_TITLE)} />
+            <PieChart
+              data={parsePieChartData(personData.ethnicity)}
+              title={getText(i18nKeys.PERSON__PIE_CHART_3_TITLE)}
+            />
           </div>
         </>
       )}
