799e36d21a95cfe39ec44e91952dbc3f1588a560
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2024-05-21 14:55:29 +0800

    Move post login redirect url context to app context (#81)

--- apps/portal/src/contexts/app-context/hooks/index.ts
+++ apps/portal/src/contexts/app-context/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from "./use-active-dataset";
 export * from "./use-feedback";
+export * from "./use-post-login-redirect-uri";
 export * from "./use-translation";
 export * from "./use-token";
 export * from "./use-user";
