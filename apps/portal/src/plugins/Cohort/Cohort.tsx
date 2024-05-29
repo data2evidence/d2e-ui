3a2507f398fe8c8e0d568eea49fd27073044e076
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-05-29 17:45:56 +0800

    refactor cohort.tsx

--- apps/portal/src/plugins/Cohort/Cohort.tsx
+++ apps/portal/src/plugins/Cohort/Cohort.tsx
@@ -10,24 +10,12 @@ interface CohortProps extends PageProps<ResearcherStudyMetadata> {}
 
 export const Cohort: FC<CohortProps> = ({ metadata }: CohortProps) => {
   const { getText, i18nKeys } = useTranslation();
-  const studyId = metadata?.studyId;
-  const userId = metadata?.userId;
-  const [dataset] = useDataset(studyId || "");
 
-  if (!userId || !studyId) {
+  const userId = metadata?.userId;
+
+  if (!userId) {
     return <div>{getText(i18nKeys.COHORT__MISSING_USER_ID)}</div>;
   }
 
-  // Initialize cohort management client object
-  const cohortMgmtClient = new CohortMgmt(studyId);
-
-  return (
-    <CohortPlugin
-      userId={userId}
-      cohortMgmtClient={cohortMgmtClient}
-      studyName={dataset?.studyDetail?.name}
-      schemaName={dataset?.schemaName}
-      databaseCode={dataset?.databaseCode}
-    />
-  );
+  return <CohortPlugin userId={userId} />;
 };
