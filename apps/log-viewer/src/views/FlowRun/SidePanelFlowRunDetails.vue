7b74ad813088ffe9b390ef682afa2f62516cadf6
Jerome Ng <jerome.ng@data4life-asia.care>
2024-05-30 00:40:43 +0800

    Add labels

--- apps/log-viewer/src/views/FlowRun/SidePanelFlowRunDetails.vue
+++ apps/log-viewer/src/views/FlowRun/SidePanelFlowRunDetails.vue
@@ -8,10 +8,15 @@ defineProps<{ flowRun: FlowRun }>()
   <div class="details-container">
     <!-- <div class="flow-run-timeline-sub-flow-run-details__heading" :crumbs="crumbs" /> -->
     <div>
+      <div>State</div>
       <div>{{ flowRun.state.type }}</div>
+      <div>Task Run ID</div>
       <div>{{ flowRun.id }}</div>
+      <div>Duration</div>
       <div>{{ flowRun.totalRunTime }} s</div>
+      <div>Created</div>
       <div>{{ flowRun.created }}</div>
+      <div>Tags</div>
       <div>{{ flowRun.tags }}</div>
     </div>
   </div>
--- apps/log-viewer/src/views/FlowRun/SidePanelTaskRunDetails.vue
+++ apps/log-viewer/src/views/FlowRun/SidePanelTaskRunDetails.vue
@@ -8,10 +8,15 @@ defineProps<{ taskRun: TaskRun }>()
   <div class="details-container">
     <!-- <div class="flow-run-timeline-sub-flow-run-details__heading" :crumbs="crumbs" /> -->
     <div>
+      <div>State</div>
       <div>{{ taskRun.state.type }}</div>
+      <div>Task Run ID</div>
       <div>{{ taskRun.id }}</div>
+      <div>Duration</div>
       <div>{{ taskRun.totalRunTime }} s</div>
+      <div>Created</div>
       <div>{{ taskRun.created }}</div>
+      <div>Tags</div>
       <div>{{ taskRun.tags }}</div>
     </div>
   </div>
