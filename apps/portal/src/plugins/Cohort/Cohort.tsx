99d5d885dac488a911b6b1429071e8792578e845
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-05-30 11:18:42 +0800

    fix cohort.tsx

--- apps/portal/src/plugins/Cohort/Cohort.tsx
+++ apps/portal/src/plugins/Cohort/Cohort.tsx
@@ -18,10 +18,10 @@ export const Cohort: FC<CohortProps> = ({ metadata }: CohortProps) => {
   if (!userId) {
     return <div>{getText(i18nKeys.COHORT__MISSING_USER_ID)}</div>;
   }
-
   return (
     <div className="cohort__container">
       <div className="cohort__content">
+        <div className="cohort__content__header">{getText(i18nKeys.COHORT_PLUGIN__COHORT_MANAGEMENT)}</div>
         {userId && (
           <CohortDefinitionList
             userId={userId}
