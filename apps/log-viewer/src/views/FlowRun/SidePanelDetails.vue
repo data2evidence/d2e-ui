be1721cf28e4895f044365028b4596e0a95eaca6
Jerome Ng <jerome.ng@data4life-asia.care>
2024-05-30 11:20:41 +0800

    add link to ids

--- apps/log-viewer/src/views/FlowRun/SidePanelDetails.vue
+++ apps/log-viewer/src/views/FlowRun/SidePanelDetails.vue
@@ -2,8 +2,19 @@
 import { FlowRun, TaskRun } from '@/types'
 import { format } from 'date-fns'
 import StateLabel from '@/components/StateLabel.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 defineProps<{ run: FlowRun | TaskRun; kind: string }>()
+
+const onClickRunId = (kind: string, id: string) => {
+  if (kind === 'flow-run') {
+    router.push(`/flowrun/${id}`)
+  } else {
+    router.push(`/taskrun/${id}`)
+  }
+}
 </script>
 
 <template>
@@ -16,7 +27,7 @@ defineProps<{ run: FlowRun | TaskRun; kind: string }>()
       </div>
       <div class="attribute">
         <div>{{ kind === 'flow-run' ? 'Flow Run ID' : 'Task Run ID' }}</div>
-        <div>{{ run.id }}</div>
+        <div class="run-id" @click="onClickRunId(kind, run.id)">{{ run.id }}</div>
       </div>
       <div class="attribute">
         <div>Duration</div>
@@ -48,5 +59,12 @@ defineProps<{ run: FlowRun | TaskRun; kind: string }>()
 }
 .attribute {
   margin-bottom: 10px;
+}
+.run-id {
+  color: #999999;
+  cursor: pointer;
+}
+.run-id:hover {
+  color: white;
 }
 </style>
