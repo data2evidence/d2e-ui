809346823780ce105ad7bed021d68d88b91a6001
Khairul Syazwan <khairul.masri@data4life-asia.care>
2024-06-14 12:31:33 +0800

    Revert "TO UNDO"
    This reverts commit 0e4d2d34bb375828b1da8c3b9b416c796634dc46.

--- apps/portal/src/components/Header/Header.tsx
+++ apps/portal/src/components/Header/Header.tsx
@@ -59,7 +59,7 @@ export const Header: FC<HeaderProps> = ({ nav, portalType, plugins, systemAdminP
           alt="Data2Evidence"
           className="logo"
           src={`${env.PUBLIC_URL}/assets/d2e.svg`}
-          // height="80px"
+          height="28"
           onClick={handleLogoClick}
         />
       </div>
