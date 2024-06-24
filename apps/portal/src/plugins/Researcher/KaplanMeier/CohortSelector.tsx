e48541e266c2b1302c857d7115b252e84cc4bf51
Jerome Ng <jerome.ng@data4life-asia.care>
2024-06-24 15:34:08 +0800

    get km results

---
+++ apps/portal/src/plugins/Researcher/KaplanMeier/CohortSelector.tsx
@@ -0,0 +1,68 @@
+import React, { FC, useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { SelectChangeEvent } from "@mui/material/Select";
+import { FormControl, InputLabel } from "@mui/material";
+import { useDatasets } from "../../../hooks";
+import { SxProps } from "@mui/system";
+import { CohortMapping, Study } from "../../../types";
+import { useTranslation } from "../../../contexts";
+interface CohortSelectorProps {
+  cohortTableName: string;
+  cohortList: CohortMapping[];
+  cohortId: number | null;
+  setCohortId: (cohortId: number) => void;
+}
+
+const styles: SxProps = {
+  color: "#000080",
+  minWidth: 220,
+  ".MuiInputLabel-root": {
+    color: "#000080",
+    "&.MuiInputLabel-shrink, &.Mui-focused": {
+      color: "var(--color-neutral)",
+    },
+  },
+  ".MuiInput-input:focus": {
+    backgroundColor: "transparent",
+    color: "#000080",
+  },
+  ".MuiInput-root": {
+    "&::after, &:hover:not(.Mui-disabled)::before": {
+      borderBottom: "2px solid #000080",
+    },
+  },
+  "&.MuiMenuItem-root:hover": {
+    backgroundColor: "#ebf2fa",
+  },
+};
+
+const CohortSelector: FC<CohortSelectorProps> = ({ cohortTableName, cohortList, cohortId, setCohortId }) => {
+  const { getText, i18nKeys } = useTranslation();
+
+  const handleCohortSelection = (event: SelectChangeEvent) => {
+    const cohortId = event.target.value;
+    setCohortId(Number(cohortId));
+  };
+
+  return (
+    <FormControl sx={styles} size="small">
+      <InputLabel id="study-selector-label">{cohortTableName}</InputLabel>
+      <Select
+        labelId="study-selector-label"
+        id="study-selector"
+        value={cohortId === null ? "" : String(cohortId)}
+        onChange={handleCohortSelection}
+        label={getText(i18nKeys.DATASET_SELECTOR__SELECT_STUDY)}
+      >
+        {cohortList.map((cohort: CohortMapping) => (
+          <MenuItem value={cohort.id} key={cohort.id} sx={styles} disableRipple>
+            {cohort.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default CohortSelector;
