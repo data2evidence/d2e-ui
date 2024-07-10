5bb0b5a14c0625393e2c92f0c10bdb5db2b64dfb
hengxian-jiang <hengxian.jiang@u.nus.edu>
2024-07-10 12:40:25 +0800

    Disable dragging and zooming, handle edge connect.

--- apps/mapping/src/Flow/Flow.tsx
+++ apps/mapping/src/Flow/Flow.tsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useCallback } from "react";
 import ReactFlow, {
-  Node,
-  ReactFlowProvider,
   addEdge,
   applyNodeChanges,
-  useEdgesState,
-  useNodesState,
+  applyEdgeChanges,
   useReactFlow,
+  Connection,
+  PanOnScrollMode,
 } from "reactflow";
 import "./Flow.scss";
 import "reactflow/dist/style.css";
@@ -27,22 +26,40 @@ const Flow = () => {
 
   console.log(reactFlow.getViewport());
 
-  const { nodes, setNodes } = useFlow();
+  const { nodes, setNodes, edges, setEdges } = useFlow();
+
+  const handleConnect = useCallback(
+    (params: Connection) => {
+      const updates = addEdge(params, edges);
+      setEdges(updates);
+    },
+    [edges]
+  );
 
   return (
     <div className="flow-container">
-      <ReactFlowProvider>
-        {/* <FlowProvider> */}
-        <ReactFlow
-          nodes={nodes}
-          nodeTypes={nodeTypes}
-          onNodesChange={(changes) =>
-            setNodes((nds) => applyNodeChanges(changes, nds))
-          }
-          fitView
-        ></ReactFlow>
-        {/* </FlowProvider> */}
-      </ReactFlowProvider>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onNodesChange={(changes) =>
+          setNodes((nds) => applyNodeChanges(changes, nds))
+        }
+        onEdgesChange={(changes) => {
+          setEdges((edges) => applyEdgeChanges(changes, edges));
+        }}
+        onConnect={handleConnect}
+        panOnScroll={true} // change default scroll
+        panOnScrollMode={PanOnScrollMode.Vertical} // only allow vertical scrolling
+        panOnDrag={false}
+        zoomOnPinch={false} // diable mouse on pinch zoom
+        translateExtent={[
+          // restrict scroll area within the canvas
+          [-500, 0],
+          [1500, 900],
+        ]}
+        fitView
+      ></ReactFlow>
     </div>
   );
 };
