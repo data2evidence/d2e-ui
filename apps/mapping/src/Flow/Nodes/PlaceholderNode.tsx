3e389ecbaa2fc5f9d1781de03b7267c00d580b34
Khairul Syazwan <khairul.masri@data4life-asia.care>
2024-07-12 16:35:36 +0800

    Enable scrolling on field nodes

--- apps/mapping/src/Flow/Nodes/PlaceholderNode.tsx
+++ apps/mapping/src/Flow/Nodes/PlaceholderNode.tsx
@@ -1,19 +1,32 @@
 import { useFlow } from "../../contexts/FlowContext";
-import { NodeProps } from "reactflow";
+import { NodeProps, useUpdateNodeInternals } from "reactflow";
 import { MappingNode } from "./MappingNode";
 import "./node.scss";
+import { debounce } from "lodash";
 
 export const PlaceholderNode = (props: NodeProps) => {
+  const updateNodeInternals = useUpdateNodeInternals();
   const { state } = useFlow();
   const { fieldTargetState, fieldSourceState } = state;
 
   const isSource = props.data.type === "source";
   const data = isSource ? fieldSourceState : fieldTargetState;
 
+  const handleWheel = debounce(() => {
+    updateNodeInternals(props.id);
+  }, 100);
+
   return (
-    <div className="link-tables__column nodrag nowheel">
+    <div
+      className="link-tables__column nodrag nowheel"
+      onWheel={() => handleWheel()}
+    >
       <div className="content-container">
-        <div className="node-container">
+        <div
+          className={
+            isSource ? "node-container node-container-source" : "node-container"
+          }
+        >
           {data.map((node) => (
             <MappingNode {...node} key={node.id} />
           ))}
