2915aed83936df84f7488a511fe4989dcf889e77
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-08-15 10:10:44 +0800

    Update main.ts

--- apps/log-viewer/src/main.ts
+++ apps/log-viewer/src/main.ts
@@ -2,18 +2,29 @@ import './assets/main.css'
 import '@prefecthq/graphs/dist/style.css'
 import 'highlight.js/styles/monokai.css'
 import '@prefecthq/prefect-design/dist/style.css'
+import '@prefecthq/prefect-ui-library/dist/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { plugin as PrefectDesign } from '@prefecthq/prefect-design'
-
+import { plugin as PrefectUILibrary } from '@prefecthq/prefect-ui-library'
+import { plugin as VueCompositionsDevtools } from '@prefecthq/vue-compositions'
 import App from './App.vue'
 import FlowRun from './views/FlowRun'
 import { createRouter, createWebHistory } from 'vue-router'
 import TaskRun from './views/TaskRun.vue'
+import Jobs from './views/Jobs'
+import AppRouterView from './views/AppRouterView.vue'
+import Sidebar from './components/Sidebar.vue'
 
 const mountLogViewer = () => {
   const routes = [
+    {
+      name: 'root',
+      path: '/',
+      components: { default: AppRouterView, sidebar: Sidebar },
+      children: [{ path: 'jobs', component: Jobs }]
+    },
     { path: '/flowrun/:flowRunId', component: FlowRun },
     { path: '/flowrun/:flowRunId/taskrun/:taskRunId', component: TaskRun }
   ]
@@ -26,6 +37,8 @@ const mountLogViewer = () => {
     app.use(createPinia())
     app.use(router)
     app.use(PrefectDesign)
+    app.use(PrefectUILibrary)
+    app.use(VueCompositionsDevtools)
     app.mount('#log-viewer-main')
   } catch (err) {
     console.log(err)
