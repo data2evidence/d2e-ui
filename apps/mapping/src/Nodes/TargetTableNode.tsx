8a0a5d34e94b0d850c7c8bbc72ce798815240ab1
suwarno ong <suwarno.ong@hotmail.com>
2024-08-20 15:37:01 +0800

    [Integrate-925] Add data type in fields mapping

--- apps/mapping/src/Nodes/TargetTableNode.tsx
+++ apps/mapping/src/Nodes/TargetTableNode.tsx
@@ -2,33 +2,41 @@ import { useCallback, useEffect, useState } from "react";
 import { NodeProps, Position, useUpdateNodeInternals } from "reactflow";
 import { debounce } from "lodash";
 import { Button } from "@mui/material";
-import { TableTargetHandleData, useTable } from "../contexts";
+import { TableSchemaState, TableTargetHandleData, useScannedSchema, useTable } from "../contexts";
 import { MappingHandle } from "./MappingHandle";
 import { api } from "../axios/api";
-import targetSourceData from "../../dummyData/5.4Version.json";
 import "./BaseNode.scss";
 
 export const TargetTableNode = (props: NodeProps) => {
   const updateNodeInternals = useUpdateNodeInternals();
   const { targetHandles, setTableTargetHandles } = useTable();
   const [cdmVersions, setCdmVersions] = useState<string[]>([]);
+  const { setCdmTables } = useScannedSchema();
 
-  // Populate version 5.4
-  const populateCDMVersion = useCallback(() => {
-    // TODO: Create other version of CDM selection
-    const data = targetSourceData;
-    const targetHandles: Partial<NodeProps<TableTargetHandleData>>[] = data.map(
-      (table, index) => ({
-        id: `C.${index + 1}`,
-        data: { label: table.table_name, tableName: table.table_name },
-        targetPosition: Position.Left,
-      })
-    );
+  const populateCDMVersion = useCallback((data: TableSchemaState[]) => {
+    const targetHandles: Partial<NodeProps<TableTargetHandleData>>[] = data.map((table, index) => ({
+      id: `C.${index + 1}`,
+      data: { label: table.table_name, tableName: table.table_name },
+      targetPosition: Position.Left,
+    }));
 
     setTableTargetHandles(targetHandles);
 
     updateNodeInternals(props.id);
   }, []);
+
+  const fetchCDMSchema = useCallback(
+    async (cdmVersion: string) => {
+      try {
+        const response = await api.backend.getCDMSchema(cdmVersion);
+        setCdmTables(response);
+        populateCDMVersion(response);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    [populateCDMVersion]
+  );
 
   const handleWheel = debounce(() => {
     updateNodeInternals(props.id);
@@ -37,9 +45,7 @@ export const TargetTableNode = (props: NodeProps) => {
   const fetchData = useCallback(async () => {
     try {
       const response = await api.backend.getCDMVersions();
-      const cdmVersions = response.sort((a: string, b: string) =>
-        a > b ? -1 : 1
-      );
+      const cdmVersions = response.sort((a: string, b: string) => (a > b ? -1 : 1));
       setCdmVersions(cdmVersions);
     } catch (e) {
       console.error(e);
@@ -66,19 +72,11 @@ export const TargetTableNode = (props: NodeProps) => {
           </div>
         ) : (
           <div className="action-container">
-            <div className="description">
-              Please select CDM version to see Target tables
-            </div>
+            <div className="description">Please select CDM version to see Target tables</div>
             <div className="button-group">
               {cdmVersions.slice(0, 2).map((cdmVersion) => {
                 return (
-                  <Button
-                    variant="contained"
-                    fullWidth
-                    onClick={
-                      cdmVersion === "5.4" ? populateCDMVersion : undefined
-                    }
-                  >
+                  <Button variant="contained" fullWidth onClick={() => fetchCDMSchema(cdmVersion)}>
                     Version {cdmVersion}
                   </Button>
                 );
---
+++ apps/mapping/src/contexts/hooks/use-scanned-schema.ts
@@ -0,0 +1,27 @@
+import { useCallback, useContext, useEffect, useState } from "react";
+import { AppContext, AppDispatchContext } from "../AppContext";
+import { ACTION_TYPES } from "../reducers/reducer";
+import { ScannedSchemaState, TableSchemaState } from "../states";
+
+export const useScannedSchema = () => {
+  const { scannedSchema } = useContext(AppContext);
+  const dispatch = useContext(AppDispatchContext);
+
+  const [sourceTables, setSourceTables] = useState<TableSchemaState[]>([]);
+
+  useEffect(() => {
+    if (scannedSchema) {
+      setSourceTables(scannedSchema.source_tables);
+    }
+  }, [scannedSchema?.source_tables]);
+
+  const setScannedSchema = useCallback((scannedSchema: ScannedSchemaState) => {
+    dispatch({ type: ACTION_TYPES.SET_SCANNED_SCHEMA, payload: scannedSchema });
+  }, []);
+
+  const setCdmTables = useCallback((cdmTables: TableSchemaState[]) => {
+    dispatch({ type: ACTION_TYPES.SET_CDM_TABLES, payload: cdmTables });
+  }, []);
+
+  return { setScannedSchema, setCdmTables, sourceTables };
+};
