e50795804a9a2cad5bc8ffcc3dd7e7582a4657eb
hengxian-jiang <155619404+hengxian-jiang@users.noreply.github.com>
2024-08-28 17:31:33 +0800

    [Integrate-1021] Support scan data from databases (#200)
    * Create UI for scanning data from postgres db
    
    * Connect white-rabbit for db table scanning
    
    * Add ConnectionErrorDialog
    
    * Removed unused import
    
    * Empty unused scss content
    
    * Remove console.log
    
    * Remove unused return
    
    * Support multiple db connection settings
    
    * optional schema name for ms-access
    
    * Fix typo

--- apps/mapping/src/axios/white-rabbit.ts
+++ apps/mapping/src/axios/white-rabbit.ts
@@ -1,4 +1,5 @@
 import request from "./request";
+import { ScanDataDBConnectionForm } from "../types/scanDataDialog";
 
 const WHITE_RABBIT_BASE_ENDPOINT = `white-rabbit/api/`;
 
@@ -35,6 +36,31 @@ export class WhiteRabbit {
     });
   }
 
+  public createDBScanReport(postgresqlForm: ScanDataDBConnectionForm, tablesToScan: string[]) {
+    const data = {
+      ...postgresqlForm,
+      scanDataParams: {
+        sampleSize: 100000,
+        scanValues: true,
+        minCellCount: 5,
+        maxValues: 1000,
+        calculateNumericStats: false,
+        numericStatsSamplerSize: 100000,
+      },
+      tablesToScan: tablesToScan.join(","),
+    };
+
+    return request({
+      baseURL: WHITE_RABBIT_BASE_ENDPOINT,
+      url: `scan-report/db`,
+      method: "POST",
+      data: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   public getScanReportProgress(id: number) {
     return request({
       url: `${WHITE_RABBIT_BASE_ENDPOINT}scan-report/conversion/${id}`,
@@ -53,6 +79,15 @@ export class WhiteRabbit {
     return request({
       url: `${WHITE_RABBIT_BASE_ENDPOINT}scan-report/result/${id}`,
       method: "GET",
+    });
+  }
+
+  public testDBConnection(connectionDetail: ScanDataDBConnectionForm) {
+    return request({
+      baseURL: WHITE_RABBIT_BASE_ENDPOINT,
+      url: `test-connection`,
+      method: "POST",
+      data: connectionDetail,
     });
   }
 }
