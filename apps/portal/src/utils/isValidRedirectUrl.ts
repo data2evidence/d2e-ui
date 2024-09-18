a3cc90966985ea46a7e634efcaa31517474df336
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2024-09-18 08:59:56 +0800

    [Integrate-1066] dashboard post login redirection fix, use dashboard-gate url (#223)
    * [Integrate-1066] dashboard post login redirect uri uses name
    
    * Use dashboard-gate URL instead of dashboard original URL

--- apps/portal/src/utils/isValidRedirectUrl.ts
+++ apps/portal/src/utils/isValidRedirectUrl.ts
@@ -1,5 +1,5 @@
 export const isValidRedirectUrl = (url: string) => {
   // Add aditional redirect urls here to be checked
-  const dashboardUrlRegex = /\/dashboard\/([0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})$/i;
+  const dashboardUrlRegex = /\/dashboard\/.+$/i;
   if (dashboardUrlRegex.test(url)) return true;
 };
