3f2532f9c602d3811b9d14db5fc03f4e441477be
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2024-09-30 16:22:17 +0800

    [Integrate 1122] refactor source/target handles type, include logic and comment in ETL model (#241)

--- apps/mapping/src/contexts/reducers/field.ts
+++ apps/mapping/src/contexts/reducers/field.ts
@@ -11,10 +11,7 @@ import {
 import { AppState } from "../states";
 import { FieldHandleData, FieldTargetHandleData } from "../states/field-state";
 
-export const setFieldNodes = (
-  state: AppState,
-  payload: NodeChange[]
-): AppState => ({
+export const setFieldNodes = (state: AppState, payload: NodeChange[]): AppState => ({
   ...state,
   field: {
     ...state.field,
@@ -22,10 +19,7 @@ export const setFieldNodes = (
   },
 });
 
-export const setFieldEdges = (
-  state: AppState,
-  payload: EdgeChange[]
-): AppState => ({
+export const setFieldEdges = (state: AppState, payload: EdgeChange[]): AppState => ({
   ...state,
   field: {
     ...state.field,
@@ -33,10 +27,7 @@ export const setFieldEdges = (
   },
 });
 
-export const addFieldConnection = (
-  state: AppState,
-  payload: Connection
-): AppState => {
+export const addFieldConnection = (state: AppState, payload: Connection): AppState => {
   const edge = {
     ...payload,
     style: {
@@ -61,24 +52,86 @@ export const addFieldConnection = (
 
 export const setFieldSourceHandles = (
   state: AppState,
-  payload: NodeProps<FieldHandleData>[]
+  payload: { tableName: string; data: NodeProps<FieldHandleData>[] }
 ): AppState => ({
   ...state,
   saved: false,
   field: {
     ...state.field,
-    sourceHandles: payload,
+    sourceHandles: {
+      ...state.field.sourceHandles,
+      [payload.tableName]: payload.data,
+    },
   },
 });
 
+export const setActiveSourceTable = (state: AppState, payload: string): AppState => ({
+  ...state,
+  saved: false,
+  field: {
+    ...state.field,
+    activeSourceTable: payload,
+  },
+});
+
+export const setActiveFieldSourceHandles = (state: AppState, payload: NodeProps<FieldHandleData>[]): AppState => {
+  if (!state.field.activeSourceTable) {
+    console.warn("Invalid operation to set field source handles when active source table is empty");
+    return state;
+  }
+
+  return {
+    ...state,
+    saved: false,
+    field: {
+      ...state.field,
+      sourceHandles: {
+        ...state.field.sourceHandles,
+        [state.field.activeSourceTable]: payload,
+      },
+    },
+  };
+};
+
 export const setFieldTargetHandles = (
   state: AppState,
-  payload: NodeProps<FieldTargetHandleData>[]
+  payload: { tableName: string; data: NodeProps<FieldTargetHandleData>[] }
 ): AppState => ({
   ...state,
   saved: false,
   field: {
     ...state.field,
-    targetHandles: payload,
+    targetHandles: {
+      ...state.field.targetHandles,
+      [payload.tableName]: payload.data,
+    },
   },
 });
+
+export const setActiveTargetTable = (state: AppState, payload: string): AppState => ({
+  ...state,
+  saved: false,
+  field: {
+    ...state.field,
+    activeTargetTable: payload,
+  },
+});
+
+export const setActiveFieldTargetHandles = (state: AppState, payload: NodeProps<FieldTargetHandleData>[]): AppState => {
+  if (!state.field.activeTargetTable) {
+    console.warn("Invalid operation to set field target handles when active target table is empty");
+    return state;
+  }
+
+  return {
+    ...state,
+    saved: false,
+    field: {
+      ...state.field,
+      targetHandles: {
+        ...state.field.targetHandles,
+        [state.field.activeTargetTable]: payload,
+      },
+    },
+  };
+};
--- apps/mapping/src/utils/utils.ts
+++ apps/mapping/src/utils/utils.ts
@@ -1,3 +1,6 @@
+import { Position } from "reactflow";
+import { FieldHandleData, TableSchemaState } from "../contexts";
+
 export const saveBlobAs = (obj: Blob, filename: string) => {
   const url = URL.createObjectURL(obj);
   const link = document.createElement("a");
@@ -9,3 +12,22 @@ export const saveBlobAs = (obj: Blob, filename: string) => {
 
   if (link.parentNode) link.parentNode.removeChild(link);
 };
+
+export const getColumns = (tables: TableSchemaState[], tableName: string) => {
+  const table = tables.find((t) => t.table_name === tableName);
+  return table?.column_list || [];
+};
+
+export const buildFieldHandle = (columnList: any[], tableName: string, isSource?: boolean) =>
+  columnList.map((column, index) => ({
+    id: `FIELD.${index + 1}`,
+    data: {
+      label: column.column_name,
+      tableName: tableName,
+      isField: true,
+      columnType: column.column_type,
+      isNullable: column.is_column_nullable,
+      type: isSource ? "input" : "output",
+    } as FieldHandleData,
+    targetPosition: isSource ? Position.Right : Position.Left,
+  }));
