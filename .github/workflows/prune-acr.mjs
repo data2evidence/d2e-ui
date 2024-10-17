868b222fb58998e56bcc25bdcf694ba980ec0119
Brian Cornally <93508235+brian-cornally@users.noreply.github.com>
2024-10-17 19:04:07 +0200

    workflow test

--- .github/workflows/prune-acr.mjs
+++ .github/workflows/prune-acr.mjs
@@ -2,7 +2,7 @@
 // https://github.com/marketplace/actions/run-zx
 // https://google.github.io/zx/faq#using-github-actions
 // $.verbose = 1; // prints commands only
-$.verbose = 2; // default; prints all output
+// $.verbose = 2; // default; prints all output
 
 const daysAgo = n => {
 	let d = new Date();
@@ -14,8 +14,16 @@ const includesAny = (arr, values) => values.some(v => arr.includes(v));
 
 const azRegPruneTimestamp = daysAgo(process.env.AZ_REG_PRUNE_DAYS).toISOString()
 
-echo(". 1 - GET manifest metadata")
-const manifestsStr = await $`az acr manifest list-metadata --only-show-errors --name $AZ_REG_REPOSITORY --registry $AZ_REG_NAME --username "$AZ_REG_USERNAME" --password "$AZ_REG_PASSWORD" --orderby time_asc --query "[?lastUpdateTime < '${azRegPruneTimestamp}']"`
+const azRegManifestsOrigYml = "private-manifests.json"
+let manifestsStr = ''
+if (fs.existsSync(azRegManifestsOrigYml)) {
+	manifestsStr = fs.readFileSync(azRegManifestsOrigYml, 'utf8');
+} else {
+	manifestsStr = await $`az acr manifest list-metadata --only-show-errors --name $AZ_REG_REPOSITORY --registry $AZ_REG_NAME --username "$AZ_REG_USERNAME" --password "$AZ_REG_PASSWORD" --orderby time_asc --query "[?lastUpdateTime < '${azRegPruneTimestamp}']"`
+	fs.writeFileSync(azRegManifestsOrigYml, manifestsStr.stdout, (err) => {
+		console.log(`ERROR writing ${azRegManifestsYml} with ${err}`);
+	})
+}
 
 const manifests = JSON.parse(manifestsStr)
 const manifestsTagged = manifests.filter(e => e.tags)
@@ -28,28 +36,37 @@ const whitelistManifests = manifestsTagged.filter(e => includesAny(e.tags, white
 const whitelistDigests = whitelistManifests.flatMap(whitelistDigest => whitelistDigest.digest)
 
 const showMetadatas = await Promise.all(whitelistManifests.map(async whitelistManifest => {
-	showMetadata = await $`az acr manifest show-metadata $AZ_REG_FQDN/$AZ_REG_REPOSITORY@${whitelistManifest.digest} --only-show-errors --username "$AZ_REG_USERNAME" --password "$AZ_REG_PASSWORD"`
-	return JSON.parse(showMetadata)
+	// showMetadata = await
+	const showMetadata = await $`az acr manifest show-metadata $AZ_REG_FQDN/$AZ_REG_REPOSITORY@${whitelistManifest.digest} --only-show-errors --username "$AZ_REG_USERNAME" --password "$AZ_REG_PASSWORD"`
+	if (typeof showMetadata == 'undefined') {
+		echo`az acr manifest show-metadata $AZ_REG_FQDN/$AZ_REG_REPOSITORY@${whitelistManifest.digest} --only-show-errors --username "$AZ_REG_USERNAME" --password "$AZ_REG_PASSWORD"`
+		throw new Error('showMetadata failed');
+	} else {
+		return JSON.parse(showMetadata)
+	}
 }))
-
 
 const referencedDigests = showMetadatas.flatMap(e => e.references).map(e => e.digest)
 
 // manifest = manifests[manifests.length - 1] // debug
-analyzedManifests = manifests.map(manifest => {
+const analyzedManifests = manifests.map(manifest => {
 	if (includesAny(manifest.digest, [...referencedDigests, ...whitelistDigests])) {
 		manifest.whitelist = true
 	}
-	showMetadata = showMetadatas.filter(showMetadata => showMetadata.digest === manifest.digest)
-	if (showMetadata.length != 0) {
-		manifest.references = showMetadata[0].references
+	const showMetadata = showMetadatas.filter(showMetadata => showMetadata.digest === manifest.digest)
+	if ((typeof showMetadata !== 'undefined') && (showMetadata.length != 0)) {
+		if (showMetadata.length != 0) {
+			manifest.references = showMetadata[0].references
+		}
+		return manifest
 	}
-	return manifest
 })
 
 const azRegManifestsYml = process.env.AZ_REG_MANIFESTS_YML
-fs.writeFileSync(azRegManifestsYml, YAML.stringify(analyzedManifests), (err) => {
-	console.log(`ERROR writing ${azRegManifestsYml} with ${err}`);
+const analyzedManifestsYAML = YAML.stringify(analyzedManifests)
+fs.writeFileSync(azRegManifestsYml, analyzedManifestsYAML, (err) => {
+	throw new Error(`writing ${azRegManifestsYml} with ${err}`);
 })
+echo(analyzedManifestsYAML)
 
 // await $`code ${azRegManifestsYml}`
