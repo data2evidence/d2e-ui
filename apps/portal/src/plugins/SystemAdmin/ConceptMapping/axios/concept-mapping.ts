39a2c7fd4b6a79fdb242df8cda826e34be4539aa
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-10-22 19:26:31 +0800

    Update concept-mapping.ts

--- apps/portal/src/plugins/SystemAdmin/ConceptMapping/axios/concept-mapping.ts
+++ apps/portal/src/plugins/SystemAdmin/ConceptMapping/axios/concept-mapping.ts
@@ -1,25 +1,24 @@
 import request from "../../../../axios/request";
 import env from "../../../../env";
-import { dataset } from "../types";
 const CONCEPT_MAPPING_URL = `${env.REACT_APP_DN_BASE_URL}concept-mapping`;
 
 export class ConceptMapping {
-  public getConceptMappings = (dataset: dataset) => {
+  public getConceptMappings = (datasetId: string) => {
     return request({
       baseURL: CONCEPT_MAPPING_URL,
       method: "GET",
       params: {
-        datasetId: dataset.datasetId,
+        datasetId: datasetId,
       },
     });
   };
 
-  public saveConceptMappings = (dataset: dataset, sourceVocabularyId: string, conceptMappings: string) => {
+  public saveConceptMappings = (datasetId: string, sourceVocabularyId: string, conceptMappings: string) => {
     return request({
       baseURL: CONCEPT_MAPPING_URL,
       method: "POST",
       params: {
-        datasetId: dataset.datasetId,
+        datasetId: datasetId,
       },
       data: {
         sourceVocabularyId: sourceVocabularyId,
