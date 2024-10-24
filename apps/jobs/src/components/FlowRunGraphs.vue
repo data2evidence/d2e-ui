f7eaa5238e638f2a68cea1f1aaf253f1d81854d4
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-10-24 12:38:51 +0800

    update flowrun component

--- apps/jobs/src/components/FlowRunGraphs.vue
+++ apps/jobs/src/components/FlowRunGraphs.vue
@@ -1,27 +1,57 @@
 <template>
   <div class="flow-run-graphs" :class="classes.root">
-    <div class="flow-run-graphs__graphs">
-      <FlowRunGraph
-        v-model:fullscreen="fullscreen"
-        v-model:viewport="dateRange"
-        v-model:selected="selectedNode"
-        :flow-run="flowRun"
-        class="flow-run-graphs__flow-run"
-      />
+    <div class="flow-run-graphs__graph-panel-container">
+      <div class="flow-run-graphs__graphs">
+        <FlowRunGraph
+          v-model:fullscreen="fullscreen"
+          v-model:viewport="dateRange"
+          v-model:selected="selection"
+          :flow-run
+          :fetch-events
+          class="flow-run-graphs__flow-run"
+        />
+      </div>
+      <div class="flow-run-graphs__panel p-background">
+        <FlowRunGraphSelectionPanel
+          v-if="selection?.kind === 'task-run' || selection?.kind === 'flow-run'"
+          v-model:selection="selection"
+          :floating="fullscreen"
+        />
+      </div>
     </div>
-    <div class="flow-run-graphs__panel p-background">
-      <FlowRunGraphSelectionPanel v-model:node="selectedNode" :floating="fullscreen" />
-    </div>
+    <FlowRunGraphEventPopover
+      v-if="selection && selection.kind === 'event'"
+      v-model:selection="selection"
+    />
+    <FlowRunGraphEventsPopover
+      v-if="selection && selection.kind === 'events'"
+      v-model:selection="selection"
+    />
+    <FlowRunGraphArtifactsPopover
+      v-if="selection && selection.kind === 'artifacts'"
+      v-model:selection="selection"
+    />
+    <FlowRunGraphStatePopover v-if="selection?.kind === 'state'" v-model:selection="selection" />
+    <FlowRunGraphArtifactDrawer v-model:selection="selection" />
   </div>
 </template>
-  
-  <script lang="ts" setup>
+
+<script lang="ts" setup>
 import {
   FlowRunGraph,
-  RunGraphNodeSelection,
+  RunGraphItemSelection,
   RunGraphViewportDateRange,
   FlowRun,
-  FlowRunGraphSelectionPanel
+  FlowRunGraphSelectionPanel,
+  FlowRunGraphArtifactDrawer,
+  FlowRunGraphArtifactsPopover,
+  FlowRunGraphStatePopover,
+  RunGraphFetchEventsContext,
+  FlowRunGraphEventPopover,
+  FlowRunGraphEventsPopover,
+  RunGraphEvent,
+  WorkspaceEventsFilter,
+  useWorkspaceApi
 } from '@prefecthq/prefect-ui-library'
 import { computed, ref } from 'vue'
 
@@ -29,34 +59,64 @@ defineProps<{
   flowRun: FlowRun
 }>()
 
+const api = useWorkspaceApi()
 const dateRange = ref<RunGraphViewportDateRange>()
 
 const fullscreen = ref(false)
-const selectedNode = ref<RunGraphNodeSelection | null>(null)
+const selection = ref<RunGraphItemSelection | null>(null)
 
 const classes = computed(() => {
   return {
     root: {
       'flow-run-graphs--fullscreen': fullscreen.value,
-      'flow-run-graphs--show-panel': Boolean(selectedNode.value)
+      'flow-run-graphs--show-panel': Boolean(
+        selection.value?.kind === 'task-run' || selection.value?.kind === 'flow-run'
+      )
     }
   }
 })
+
+const fetchEvents = async ({
+  nodeId,
+  since,
+  until
+}: RunGraphFetchEventsContext): Promise<RunGraphEvent[]> => {
+  const filter: WorkspaceEventsFilter = {
+    anyResource: {
+      id: [`prefect.flow-run.${nodeId}`]
+    },
+    event: {
+      excludePrefix: ['prefect.log.write', 'prefect.task-run.']
+    },
+    occurred: {
+      since,
+      until
+    }
+  }
+
+  const { events } = await api.events.getEvents(filter)
+
+  return events
+}
 </script>
-  
-  <style>
+
+<style>
 .flow-run-graphs {
-  @apply relative
-    grid
-    grid-cols-1
-    gap-2
-    overflow-hidden;
+  @apply relative;
   --flow-run-graphs-panel-width: 320px;
+}
+
+.flow-run-graphs__graph-panel-container {
+  @apply relative
+  grid
+  grid-cols-1
+  gap-2
+  overflow-hidden;
 }
 
 .flow-run-graphs--fullscreen {
   @apply z-20
-    static;
+  static;
 }
 
 .flow-run-graphs__graphs {
@@ -70,29 +130,27 @@ const classes = computed(() => {
 
 .flow-run-graphs__flow-run {
   @apply overflow-hidden
-    rounded;
+  rounded;
 }
 
 .flow-run-graphs__panel {
   @apply absolute
-    right-0
-    top-0
-    bottom-0
-    translate-x-full
-    transition-transform
-    rounded;
+  right-0
+  top-0
+  bottom-0
+  translate-x-full
+  transition-transform
+  rounded;
   width: var(--flow-run-graphs-panel-width);
 }
 
 .flow-run-graphs--fullscreen .flow-run-graphs__panel {
-  @apply
-    top-4
-    right-4
-    bottom-auto;
+  @apply top-4
+  right-4
+  bottom-auto;
 }
 
 .flow-run-graphs--show-panel .flow-run-graphs__panel {
   @apply translate-x-0;
 }
 </style>
-  
