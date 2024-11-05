6ccbb0cdbed1d2d6477afa1ead52b6ded8d9f56d
suwarno ong <suwarno.ong@hotmail.com>
2024-11-05 12:20:07 +0800

    1200 remove plugin table & plugin initialise menu

--- apps/portal/src/plugins/Setup/PluginTable/module.ts
+++
@@ -1,4 +0,0 @@
-import { SetupPagePlugin } from "@portal/plugin";
-import { PluginTable } from "./PluginTable";
-
-export const plugin = new SetupPagePlugin(PluginTable);
--- apps/portal/src/plugins/builtInPlugins.ts
+++ apps/portal/src/plugins/builtInPlugins.ts
@@ -28,7 +28,6 @@ const moduleNames = [
   "Setup/Db",
   "Setup/HybridSearch",
   "Setup/OverviewDescription",
-  "Setup/PluginTable",
   "Setup/TrexPlugins",
 ];
 
