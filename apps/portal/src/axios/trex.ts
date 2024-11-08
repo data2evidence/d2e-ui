4ce7c40105191cb23b8b896b7b2aecf12f7c77b7
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2024-11-08 10:01:23 +0800

    [Integrate-1200] manage trex plugin (#272)
    * 1200 manage trex plugin
    
    * 1200 remove plugin table & plugin initialise menu
    
    * 1200 rename delete to uninstall in trex plugin
    
    * 1200 remove unused dependency
    
    * 1200 fix formatting
    
    * 1200 add manage trex plugin menu, sync ui plugin json

---
+++ apps/portal/src/axios/trex.ts
@@ -0,0 +1,39 @@
+import request from "./request";
+import env from "../env";
+import { TrexPlugin } from "../types";
+
+const TREX_URL = `${env.REACT_APP_DN_BASE_URL}trex/`;
+
+export class Trex {
+  public getPlugins() {
+    return request<TrexPlugin[]>({
+      baseURL: TREX_URL,
+      url: "plugins",
+      method: "GET",
+    });
+  }
+
+  public installPlugin(name: string) {
+    return request<TrexPlugin>({
+      baseURL: TREX_URL,
+      url: `plugins/${name}`,
+      method: "POST",
+    });
+  }
+
+  public updatePlugin(name: string) {
+    return request<TrexPlugin>({
+      baseURL: TREX_URL,
+      url: `plugins/${name}`,
+      method: "PUT",
+    });
+  }
+
+  public uninstallPlugin(name: string) {
+    return request<{ message: string }>({
+      baseURL: TREX_URL,
+      url: `plugins/${name}`,
+      method: "DELETE",
+    });
+  }
+}
