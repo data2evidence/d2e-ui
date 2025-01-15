6b8665086336dff0135d05ef705d865f35d3e9e5
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-01-15 18:00:11 +0800

    Update useDbVocabSchemas.ts

--- apps/portal/src/hooks/useDbVocabSchemas.ts
+++ apps/portal/src/hooks/useDbVocabSchemas.ts
@@ -18,7 +18,9 @@ export const useDbVocabSchemas = (dialect: string): [{ [key: string]: string[] }
   );
 
   useEffect(() => {
-    getDbVocabSchemas(dialect);
+    if (dialect.length > 0) {
+      getDbVocabSchemas(dialect);
+    }
   }, [dialect, getDbVocabSchemas]);
 
   return [dbVocabSchemas];
