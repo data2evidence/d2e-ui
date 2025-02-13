e44df20333d93644e81e2ea213444ed563b4ed7a
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2025-02-13 13:55:24 +0800

    [Integrate-1366] add authentication mode in database setting (#359)
    * 1366 add authentication mode in database setting
    
    * 1366 hide authentication mode if not hana

--- apps/portal/src/plugins/Setup/Db/CredentialValidator.ts
+++ apps/portal/src/plugins/Setup/Db/CredentialValidator.ts
@@ -12,12 +12,13 @@ export function validateCredentials(
       type: "error",
       message: "Please ensure all passwords to have valid length",
     });
-    return;
+    return false;
   } else if (!passwords.every((p) => p.length <= maxLength)) {
     setFeedback({
       type: "error",
       message: `Please ensure all passwords to have the maximum length of ${maxLength}`,
     });
-    return;
+    return false;
   }
+  return true;
 }
